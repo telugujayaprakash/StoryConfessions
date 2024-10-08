2de8e8bff85cd94527f88a657b849063573e91b8
telugujayaprakash <jayaprakash96030@gmail.com>
2024-10-08 19:30:23 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <footer className="bg-gray-900 text-gray-300 py-2">
       <div className="container mx-auto flex flex-col items-center">
         <div className="mt-6 text-center">
-          <h3 className="text-2xl font-bold text-white mb-2">Jayaprakash</h3>
-          <p className="text-gray-400 text-sm">Copyright © 2024</p>
+          <h3 className="text-2xl font-bold text-white mb-2">Are these stories Good..?? and Why can't you upload one</h3>
+          <p className="text-gray-400 text-sm">Designed & Developed by JP</p>
         </div>
       </div>
     </footer>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -75,7 +75,7 @@ function Home() {
                                                 <div className="flex items-center flex-wrap ">
                                                     <Link to={'screens'}>
                                                         <span onClick={() => { setcategory(c.category) }} className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
-                                                            Learn More
+                                                            Explore Here
                                                             <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                                                 <path d="M5 12h14"></path>
                                                                 <path d="M12 5l7 7-7 7"></path>
